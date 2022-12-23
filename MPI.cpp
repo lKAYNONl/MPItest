5c9baf2fc32488898ae8436c5121d7440b25fede
KAYNOMI <79145281124@yandex.ru>
2022-12-23 14:46:31 +0900

    боль

--- MPI.cpp
+++ MPI.cpp
@@ -3,10 +3,84 @@
 #include "mpi.h"
 #include <iostream>
 
-int main()
+int main(int argc, char** argv)
 {
-    std::cout << "Hello World!\n";
+    const int MAX = 20;
+    int rank, size;
+    int n, ibeg, iend;
+
+    MPI_Init(&argc, &argv);
+    
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    
+    n = (MAX - 1) / size + 1;
+    ibeg = rank * n + 1;
+    iend = (rank + 1) * n;
+    for (int i = ibeg; i <= ((iend > MAX) ? MAX : iend); i++)
+    {
+        printf("Process: %d, %d^2=%d\n", rank, i, i * i);
+    }
+
+    //// Размер массива
+    //const int N = 15;
+    //// Массив с элементами
+    //int data[N];
+    //// Заполняем массив случайными числами
+    //for (int i = 0; i < N; i++)
+    //{
+    //    data[i] = -10 + rand() % ((10 + 1) +10);;
+    //    printf("Process: %d\n",data[i]);
+
+    //}
+    //    
+
+    //// Произведение отрицательных элементов
+    //int local_product = 1;
+    //// Разбиваем массив на части и распределяем по процессам
+    //for (int i = rank; i < N; i += size)
+    //    if (data[i] < 0)
+    //    {
+    //        local_product *= data[i];
+    //        printf("Process: %d, *%d=%d\n", rank, data[i], local_product);
+    //    
+    //    }
+    //        
+
+    //// Собираем результаты от всех процессов
+    //int global_product;
+    //MPI_Reduce(&local_product, &global_product, 1, MPI_INT, MPI_PROD, 0, MPI_COMM_WORLD);
+
+    //if (rank == 0)
+    //    std::cout << "Global product: " << global_product << std::endl;
+
+    MPI_Finalize();
+    return 0;
 }
+
+
+//int main(int argc, char** argv)
+//{
+//	const int MAX = 200;
+//	int rank, size;
+//	int n, ibeg, iend;
+//
+//	MPI_Init(&argc, &argv);
+//	MPI_Comm_size(MPI_COMM_WORLD, &size);
+//	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+//
+//	n = (MAX - 1) / size + 1;
+//	ibeg = rank * n + 1;
+//	iend = (rank + 1) * n;
+//	for (int i = ibeg; i <= ((iend > MAX) ? MAX : iend); i++)
+//	{
+//		printf("Process: %d, %d^2=%d\n", rank, i, i * i);
+//	}
+//
+//	MPI_Finalize();
+//
+//	return 0;
+//}
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
 // Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
